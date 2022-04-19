9cbb774dcf8dc4e94ef00e1c96a8c22468431aaf
Poetics <87587068+ThePoetics@users.noreply.github.com>
2022-04-18 21:56:40 -0700

    Added Texture

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,12 @@
 Hooks.once('diceSoNiceReady', (dice3d) => {
-        dice3d.addSystem({id: "SRSN", name: "Shadowrun so Nice"});
+        dice3d.addTexture("gmetal", {
+			name: "Gunmetal",
+			composite: "source-over",
+			source: "modules/sr-so-nice/images/texture.png",
+			bump: ""
+		});
+		
+		dice3d.addSystem({id: "SRSN", name: "Shadowrun so Nice"});
         dice3d.addDicePreset({
                 type: "d6",
                 labels: ["modules/sr-so-nice/images/glitch.png","","","","modules/sr-so-nice/images/hit.png","modules/sr-so-nice/images/hit.png"],
@@ -11,6 +18,7 @@ Hooks.once('diceSoNiceReady', (dice3d) => {
                 description: 'Shadowrun so Nice',
                 category: "Shadowrun",
                 material: "metal",
+				texture: 'gmetal',
                 background: "#5C5D5B",
                 system: "SRSN"
         });
