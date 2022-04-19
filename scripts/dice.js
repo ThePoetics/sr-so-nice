79c7c1e4ca6ca9f9e136f31f35b1c1060e79d285
Poetics <87587068+ThePoetics@users.noreply.github.com>
2022-04-18 18:44:22 -0700

    Create dice.js
    Defined labels for the D6 as well as material and color scheme

---
+++ scripts/dice.js
@@ -0,0 +1,19 @@
+Hooks.once('diceSoNiceReady', (dice3d) => {
+        dice3d.addSystem({id: "SRSN", name: "Shadowrun so Nice"}, preferred);
+        dice3d.addDicePreset({
+                type: "d6",
+                labels: ["modules/sr-so-nice/images/glitch.png","","","","modules/sr-so-nice/images/hit.png","modules/sr-so-nice/images/hit.png"],
+                bumpmaps: ["modules/sr-so-nice/images/glitch-bm.png","","","","modules/sr-so-nice/images/hit-bm.png","modules/sr-so-nice/images/hit-bm.png"],
+                system: "SRSN"
+        });
+  
+        dice3d.addColorset({
+                name: 'SRSN',
+                description: 'Shadowrun so Nice',
+                category: "Shadowrun",
+                material: "metal",
+                background: "#5C5D5B",
+                system: "SRSN"
+        });
+  
+});
